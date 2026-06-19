1e75d0cadf1328b5fc19768b2f38d07017f43b3f
Yogesh Rathod <itsyogeshrathod.14@gmail.com>
2026-06-19 18:22:02 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,49 @@
+SELECT m.name,
+       b.title,
+       br.issue_date
+FROM Borrow_Records br
+JOIN Members m
+ON br.member_id = m.member_id
+JOIN Books b
+ON br.book_id = b.book_id;
 
+SELECT b.title,
+       COUNT(*) AS borrow_count
+FROM Borrow_Records br
+JOIN Books b
+ON br.book_id = b.book_id
+GROUP BY b.title
+ORDER BY borrow_count DESC;
+
+SELECT m.name,
+       COUNT(*) AS books_borrowed
+FROM Borrow_Records br
+JOIN Members m
+ON br.member_id = m.member_id
+GROUP BY m.name
+ORDER BY books_borrowed DESC;
+
+SELECT m.name,
+       b.title,
+       br.issue_date
+FROM Borrow_Records br
+JOIN Members m
+ON br.member_id = m.member_id
+JOIN Books b
+ON br.book_id = b.book_id
+WHERE br.status='Not Returned';
+
+CREATE VIEW Popular_Books AS
+SELECT b.title,
+       COUNT(*) total_borrowed
+FROM Borrow_Records br
+JOIN Books b
+ON br.book_id=b.book_id
+GROUP BY b.title;
+
+CREATE TRIGGER update_copies
+AFTER INSERT ON Borrow_Records
+FOR EACH ROW
+UPDATE Books
+SET available_copies = available_copies - 1
+WHERE book_id = NEW.book_id;
